e5184a9a90128065bd4db2c780ebdd14cb169299
kimkh0422 <kkh042299@naver.com>
2025-07-03 21:34:26 +0900

    실전! 스프링 부트와 JPA활용1 복습 프로젝트

---
+++ src/main/java/jpabookRepeat/jpashopRepeat/JpashopRepeatApplication.java
@@ -0,0 +1,13 @@
+package jpabookRepeat.jpashopRepeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpashopRepeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpashopRepeatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jpabookRepeat/jpashopRepeat/JpashopRepeatApplicationTests.java
@@ -0,0 +1,13 @@
+package jpabookRepeat.jpashopRepeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpashopRepeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
